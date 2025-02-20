e3ec3f5412396a3f5ae39a7b0783b4be86146013
nidatedilla <nidatedilla@gmail.com>
2025-02-21 00:36:03 +0700

    fix: error order controller

--- prisma/migrations/20250215235940_add_area_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `area_id` to the `locations` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "locations" ADD COLUMN     "area_id" TEXT NOT NULL;
