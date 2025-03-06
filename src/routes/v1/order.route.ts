72a64ea260643af83a612e4711fbc505560c6901
Nida Tedilla <138346673+nidatedilla@users.noreply.github.com>
2025-03-06 19:21:47 +0700

    Update order.route.ts

--- src/routes/v1/order.route.ts
+++ src/routes/v1/order.route.ts
@@ -17,15 +17,15 @@ import { createBiteshipOrder } from '../../services/biteship.service';
 
 const orderRouter = Router();
 
-orderRouter.get('/', auth, getStoreOrders);
-orderRouter.get('/total-revenue', auth, getTotalRevenueByStoreHandler);
-orderRouter.get('/today', auth, getTotalOrdersTodayByStoreHandler);
-orderRouter.get('/:orderId', auth, getOrder);
-orderRouter.get('/tracking/:orderId', getOrderById);
-orderRouter.post('/create', createOrder);
 orderRouter.post('/webhook', biteshipWebhook);
 orderRouter.post('/couriers/rates', fetchCourierRates);
 orderRouter.post('/webhook-midtrans', midtransWebhook);
 orderRouter.post('/create-biteship', createBiteshipOrder);
+orderRouter.get('/tracking/:orderId', getOrderById);
+orderRouter.post('/create', createOrder);
+orderRouter.get('/', auth, getStoreOrders);
+orderRouter.get('/total-revenue', auth, getTotalRevenueByStoreHandler);
+orderRouter.get('/today', auth, getTotalOrdersTodayByStoreHandler);
+orderRouter.get('/:orderId', auth, getOrder);
 
 export default orderRouter;
